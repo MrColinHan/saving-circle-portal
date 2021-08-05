c5394dbdd9fb85c33cee23c58c2de3e95a1e449f
MrColinHan <changzehan@yahoo.com>
2021-08-04 20:58:59 -0500

    fix loan_request chart functions and variables, add active user tracking feature

--- portal/views.py
+++ portal/views.py
@@ -41,6 +41,7 @@ def login_view(request):
             return redirect('/main_page')
     else:
         login_form = AuthenticationForm()
+
     return render(request, 'portal/login.html', {'login_form': login_form})
 
 
@@ -91,13 +92,22 @@ def pass_python_data_toHTML(request):
         amount
         timeStamp
     }
+    loanRequestMadeEntities {
+        id
+        requester
+        circle
+        amount
+        timeStamp
+        granted
+    }
     }
     ''')
 
     graphql_query_response = client.execute(graphql_query)
 
-    '''===save "circle" data to a dataframe==='''
+
 # =================================== save "circle" data to a dataframe ===================================
+    '''===save "circle" data to a dataframe==='''
     print(f"====== Pulling Circle Data ======")
     circles_df = pd.DataFrame(columns=['circle address', 'creator address', 'timeStamp'])
     # print(f"{len(graphql_query_response['circleCreatedEntities'])} circles have been created: ")
@@ -105,15 +115,20 @@ def pass_python_data_toHTML(request):
         circles_df.loc[len(circles_df)] = [circle['id'], circle['creator'],
                                            datetime.fromtimestamp(int(circle['timeStamp']))]
         # print(f"{circle['id']} ----- {circle['creator']} ----- {datetime.fromtimestamp(int(circle['timeStamp']))}")
-    print(circles_df)
+    #print(circles_df)
     circles_df.to_csv("csv_files_fromPy/circles_created.csv")  # output circle dataframe to a csv file
     # === count number of circles by day
     circle_count_byDay_df = circles_df.groupby(pd.Grouper(key='timeStamp', freq='D'))['circle address'].count()
     circle_count_byDay_df.to_frame().rename(columns={'circle address': 'count'}).to_csv(
         "csv_files_fromPy/circle_count_byDay.csv")  # output this df to a csv file
 
-    '''===save "deposit_made" data to a dataframe==='''
+    circle_count_byMonth_df = circles_df.groupby(pd.Grouper(key='timeStamp', freq='M'))['circle address'].count()
+    circle_count_byMonth_df.to_frame().rename(columns={'circle address': 'count'}).to_csv(
+        "csv_files_fromPy/circle_count_byMonth.csv")  # output this df to a csv file
+
+
 # =================================== save "deposit" data to a dataframe ===================================
+    '''===save "deposit_made" data to a dataframe==='''
     print(f"====== Pulling Deposit_made Data ======")
     deposit_df = pd.DataFrame(columns=['id', 'depositor', 'circle', 'amount', 'timeStamp'])
     for deposit in graphql_query_response['depositMadeEntities']:
@@ -121,22 +136,35 @@ def pass_python_data_toHTML(request):
                                            deposit['depositor'],
                                            deposit['circle'], deposit['amount'],
                                            datetime.fromtimestamp(int(deposit['timeStamp']))]
-    print(deposit_df)
+    #print(deposit_df)
     total_deposit_amount_dollar = deposit_df['amount'].astype(int).sum()
     deposit_df.to_csv("csv_files_fromPy/deposits_made.csv")  # save as a csv file
     # === count number of deposits each day
     deposit_count_byDay_df = deposit_df.groupby(pd.Grouper(key='timeStamp', freq='D'))['id'].count()
     deposit_count_byDay_df.to_frame().rename(columns={'id': 'count'}).to_csv("csv_files_fromPy/deposit_count_byDay.csv")
 
-    '''===save "loan request" data to a dataframe==='''
+
 # =================================== save "request" data to a dataframe ===================================
+    '''===save "loan request" data to a dataframe==='''
     print(f"====== Pulling_request Data ======")
+    request_df = pd.DataFrame(columns=['id', 'requester', 'circle', 'amount', 'timeStamp', 'granted'])
+    for loan_request in graphql_query_response['loanRequestMadeEntities']:
+        request_df.loc[len(request_df)] = [loan_request['id'],
+                                           loan_request['requester'],
+                                           loan_request['circle'], loan_request['amount'],
+                                           datetime.fromtimestamp(int(loan_request['timeStamp'])),
+                                           loan_request['granted']]
+    # print(deposit_df)
+    total_request_amount_dollar = request_df['amount'].astype(int).sum()
+    request_df.to_csv("csv_files_fromPy/requests_made.csv")  # save as a csv file
+    # === count number of requests each day
+    request_count_byDay_df = request_df.groupby(pd.Grouper(key='timeStamp', freq='D'))['id'].count()
+    request_count_byDay_df.to_frame().rename(columns={'id': 'count'}).to_csv("csv_files_fromPy/request_count_byDay.csv")
 
 
-
+# =================================== parse circle count data to chart.js ===================================
     '''===passing data to js==='''
     print(f"====== passing data to js ======")
-# =================================== parse circle count data to chart.js ===================================
     # convert circle datetime to str-date
     circle_created_dates_list = circle_count_byDay_df.to_frame().rename(columns={'circle address': 'count'}).index.to_list()
     str_circle_created_dates_list = list()  # all days
@@ -161,6 +189,10 @@ def pass_python_data_toHTML(request):
         circle_date_count_dict_90days[k] = circle_date_count_dict[k]
         circle_date_count_dict_365days[k] = circle_date_count_dict[k]
 
+    # important
+    '''NEED TO UPDATE THIS PART'''
+    all_active_users_list = ['0xdfdfb95cfed85320ead3713daa4f4de1f7ca05ee']
+
 # =================================== parse deposit count data to chart.js ===================================
     # convert deposit datetime to str-date
     deposit_created_dates_list = deposit_count_byDay_df.to_frame().rename(columns={'id': 'count'}).index.to_list()
@@ -182,10 +214,33 @@ def pass_python_data_toHTML(request):
         deposit_date_count_dict_90days[k] = deposit_date_count_dict[k]
         deposit_date_count_dict_365days[k] = deposit_date_count_dict[k]
 
-# =================================== parse request count data to chart.js ===================================
 
+
+# =================================== parse request count data to chart.js ===================================
+    # convert request datetime to str-date
+    request_created_dates_list = request_count_byDay_df.to_frame().rename(columns={'id': 'count'}).index.to_list()
+    str_request_created_dates_list = list()  # all days
+    for i in request_created_dates_list:
+        str_request_created_dates_list.append(i.strftime('%m-%d-%y'))
+
+    request_dailyCount_list = request_count_byDay_df.to_frame().rename(columns={'id': 'count'})['count'].to_list()
+
+    # IMPORTANT
+    request_date_count_dict = dict(zip(str_request_created_dates_list, request_dailyCount_list))
+
+    # IMPORTANT
+    request_date_count_dict_30days = copy.deepcopy(empty_30days_dict)
+    request_date_count_dict_90days = copy.deepcopy(empty_90days_dict)
+    request_date_count_dict_365days = copy.deepcopy(empty_365days_dict)
+    for k in request_date_count_dict:
+        request_date_count_dict_30days[k] = request_date_count_dict[k]
+        request_date_count_dict_90days[k] = request_date_count_dict[k]
+        request_date_count_dict_365days[k] = request_date_count_dict[k]
+
+# =================================== pass all data to chart.js ===================================
     pass_data = {
         'amount_of_circles': len(graphql_query_response['circleCreatedEntities']),
+        'amount_of_users': len(all_active_users_list),
 
         'circle_created_dates_list': circle_date_count_dict.keys(),
         'circle_dailyCount_list': circle_date_count_dict.values(),
@@ -207,6 +262,18 @@ def pass_python_data_toHTML(request):
         '90days_deposit_dailyCount_list': deposit_date_count_dict_90days.values(),
         '365days_deposit_created_dates_list': deposit_date_count_dict_365days.keys(),
         '365days_deposit_dailyCount_list': deposit_date_count_dict_365days.values(),
+
+        'amount_of_requests': len(graphql_query_response['loanRequestMadeEntities']),
+        'dollar_amount_of_requests': total_deposit_amount_dollar,
+
+        'request_created_dates_list': request_date_count_dict.keys(),
+        'request_dailyCount_list': request_date_count_dict.values(),
+        '30days_request_created_dates_list': request_date_count_dict_30days.keys(),
+        '30days_request_dailyCount_list': request_date_count_dict_30days.values(),
+        '90days_request_created_dates_list': request_date_count_dict_90days.keys(),
+        '90days_request_dailyCount_list': request_date_count_dict_90days.values(),
+        '365days_request_created_dates_list': request_date_count_dict_365days.keys(),
+        '365days_request_dailyCount_list': request_date_count_dict_365days.values(),
 
         '30days_list': get_x_dayList(30),
         '90days_list': get_x_dayList(90),
